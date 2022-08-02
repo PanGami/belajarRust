59ac68e34b16cd6bd8e5b222389e54320880446d
Pan <pancanugrahawicaksana@gmail.com>
2022-08-03 06:21:17 +0700

    in progress

--- batu_kertas_gunting/src/main.rs
+++ batu_kertas_gunting/src/main.rs
@@ -1,3 +1,47 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let pilihan_user = user_input();
+    // processing(pilihan_user);
+}
+
+fn processing(user_input: char){
+    print!("{}", user_input);
+}
+
+fn user_input(){    
+    let mut user_input = String::new();    
+    loop{       
+        println!("Masukkan huruf awal pilihan! ");
+        println!("Kertas : k, Gunting : g, Batu : b");
+
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Gagal membaca input!");    
+                
+        // println!("TEST CONVERTED INPUT ! {}", converted_input);
+        // println!("TEST USER INPUT ! {}", user_input);
+        let mut converted_input = user_input.to_lowercase().chars().next().unwrap();
+        match converted_input {
+            'b' =>{
+                println!("Anda memilih batu!");
+                break;
+            },
+            'k' =>{
+                println!("Anda memilih kertas!");
+                break;
+            },
+            'g' =>{
+                println!("Anda memilih gunting!");
+                break;
+            },
+            _ =>{
+                println!("Input tidak valid!");
+                continue;
+            }
+        }                
+        user_input;     
+        break;   
+    }
 }
