454e0fc3bde2d73de52c67c0dcb6be96aa712ce1
Pan <pancanugrahawicaksana@gmail.com>
2022-08-04 18:57:51 +0700

    Add formatter

--- batu_kertas_gunting/src/main.rs
+++ batu_kertas_gunting/src/main.rs
@@ -3,13 +3,18 @@ use rand::Rng;
 
 fn main() {
     let pilihan_user = user_input();
-    println!("Test {}", pilihan_user);
+    let pilihan_komputer = komputer_input();
+
+    println!("Pilihan user: {}", formatter(pilihan_user));
+    println!("Pilihan komputer: {}", formatter(pilihan_komputer));
+    
+    //let hasil = hasil_game(pilihan_user, pilihan_komputer);
 }
 
 fn user_input() -> u32{    
     let value;     
     loop{               
-        println!("Input pilihan anda! Kertas[0], Gunting[1], Batu[2]");
+        println!("Input pilihan anda! Batu[0], Kertas[1], Gunting[2]");
         let mut user_input = String::new();   
         io::stdin()
             .read_line(&mut user_input)
@@ -34,7 +39,17 @@ fn user_input() -> u32{
     return value;
 }
 
-fn computer_input() -> u32{
-    let mut random_number = rand::thread_rng().gen_range(0..=2);    
+fn komputer_input() -> u32{
+    let random_number = rand::thread_rng().gen_range(0..=2);    
     return random_number;
+}
+
+fn formatter(pilihan_angka: u32) -> String{
+    let hasil = match pilihan_angka {
+        0 => "Batu".to_string(),
+        1 => "Kertas".to_string(),
+        2 => "Gunting".to_string(),
+        _ => "Tidak ada".to_string(),
+    };
+    return hasil;
 }
